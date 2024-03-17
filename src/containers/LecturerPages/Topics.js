3052962cc0f6d6a66469f2e3bf9ea688f12c79b0
viet003 <tansinhvien03@gmail.com>
2024-03-17 14:08:00 +0700

    update

--- src/containers/LecturerPages/Topics.js
+++ src/containers/LecturerPages/Topics.js
@@ -98,7 +98,7 @@ const Topic = () => {
                     }
                     break;
                 case 'quantity':
-                    if(isNaN(item[1])) {
+                    if (isNaN(item[1])) {
                         setInvalidFields(prev => [...prev, {
                             name: item[0],
                             message: 'số lượng chỉ bao gồm ký tự số'
@@ -283,17 +283,21 @@ const Topic = () => {
                         </div>
                     )
                 }
-                <div className="flex justify-between items-center">
-                    <div className="flex justify-around items-center">
-                        <div class="z-0 px-4 h-[35px] rounded-md tracking-wider border-[1px] w-[200px] border-gray-300 hover:border-primary hover:text-primary transition-all duration-200 cursor-pointer flex items-center">
-                            <input type="text" class="m-auto w-full outline-none text-[13px]" placeholder="Tìm kiếm..." value={search} onChange={(e) => setSearch(e.target.value)} />
-                            <GoSearch />
+                <div className="flex flex-col lg:flex-row md:justify-between items-center ">
+                    <div className="flex flex-col lg:flex-row gap-2 items-center">
+                        <div className="px-5 lg:px-0 w-full phone:w-[400px] lg:w-[200px]">
+                            <div class="z-0 px-4 h-[35px] rounded-md tracking-wider border-[1px] w-full border-gray-300 hover:border-primary hover:text-primary transition-all duration-200 cursor-pointer flex items-center">
+                                <input type="text" class="m-auto w-full outline-none text-[13px]" placeholder="Tìm kiếm..." value={search} onChange={(e) => setSearch(e.target.value)} />
+                                <GoSearch />
+                            </div>
                         </div>
-                        <button onClick={() => { setAdd(true); }} class="ml-5  w-[100px] bg-gray-100 border-gray-200 border rounded-xl text-primary py-[6px] hover:text-white hover:bg-primary duration-100 ">Add+</button>
-                        <button onClick={() => { setEdit(prev => !prev); }} class=" ml-5  w-[100px] bg-gray-100 border-gray-200 border rounded-xl text-primary py-[6px] hover:text-white hover:bg-primary duration-100">Edit</button>
-                        <button onClick={() => { setDel(prev => !prev); }} class=" ml-5  w-[100px] bg-gray-100 border-gray-200 border rounded-xl text-primary py-[6px] hover:text-white hover:bg-primary duration-100">Delete</button>
+                        <div className="flex w-full phone:w-[400px] justify-center items-center">
+                            <button onClick={() => { setAdd(true); }} class="w-[100px] bg-gray-100 border-gray-200 border rounded-xl text-primary py-[6px] hover:text-white hover:bg-primary duration-100 ">Add+</button>
+                            <button onClick={() => { setEdit(prev => !prev); }} class=" ml-5  w-[100px] bg-gray-100 border-gray-200 border rounded-xl text-primary py-[6px] hover:text-white hover:bg-primary duration-100">Edit</button>
+                            <button onClick={() => { setDel(prev => !prev); }} class=" ml-5  w-[100px] bg-gray-100 border-gray-200 border rounded-xl text-primary py-[6px] hover:text-white hover:bg-primary duration-100">Delete</button>
+                        </div>
                     </div>
-                    <div className="relative flex flex-col items-center py-2 w-[130px] min-w-[130px] 2xl:w-[200px] rounded-lg" onClick={() => setOpen((prev) => !prev)}>
+                    <div className="relative flex flex-col px-5 lg:px-0 items-center py-2 min-w-[130px] w-full phone:w-[400px] xl:w-[200px] rounded-lg" onClick={() => setOpen((prev) => !prev)}>
                         <div className="w-full relative">
                             <button className="p-4 z-0 h-[35px] flex w-full items-center justify-between text-[13px] rounded-md tracking-wider border-[1px] border-gray-300  hover:border-primary hover:text-primary transition-all duration-200">
                                 {value}
@@ -347,7 +351,7 @@ const Topic = () => {
                                             del && (
                                                 <div className="absolute -top-[1.2rem] flex items-center justify-center -right-4 w-10 h-10">
                                                     <IoClose className={`cursor-pointer text-[25px] text-white rounded-full ${listColorsBg[colorIndex]} hover:text-orange-400`}
-                                                        onClick={() => {deleteTopic(element.id)}} />
+                                                        onClick={() => { deleteTopic(element.id) }} />
                                                 </div>
                                             )
                                         }
