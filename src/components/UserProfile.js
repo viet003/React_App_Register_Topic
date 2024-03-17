d0ac4b2edb021dcdad0a82fc9267cedadabd20fd
viet003 <tansinhvien03@gmail.com>
2024-03-17 15:30:23 +0700

    update

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/admin.jpg"
+import admin from "../assets/admin.jpg"
 import user from "../assets/user.jpg"
 import { useSelector } from "react-redux";
 import { jwtDecode } from "jwt-decode";
@@ -10,12 +10,12 @@ const UserProfile = ({ toggle }) => {
     const { token } = useSelector(state => state.auth)
     const id = token ? jwtDecode(token).id : "No user"
     const name = token ? jwtDecode(token).name : "Admin"
-    const type = token ? jwtDecode(token).type : "Admin"
+    const type = token ? jwtDecode(token).type : ""
     // console.log(jwtDecode(token).name)
     return (
         <div className={`flex items-center gap-5 py-3 rounded-xl ${toggle ? "bg-none transition-all " : "bg-white"}`}>
             <div className="min-w-[3.5rem] h-[3rem] flex items-center justify-around overflow-hidden">
-                <img src={name == 'Admin' ? logo : user} alt="" className={`w-[3rem] h-full rounded-full object-cover ${toggle ? "text-white transition-all" : "text-black"}`} />
+                <img src={type === 'Quản trị viên' ? admin : user} alt="" className={`w-[3rem] h-full rounded-full object-cover ${toggle ? "text-white transition-all" : "text-black"}`} />
             </div>
             <div className={toggle ? "opacity-0 transition-opacity text-white" : "opacity-100 duration-300"}>
                 {
